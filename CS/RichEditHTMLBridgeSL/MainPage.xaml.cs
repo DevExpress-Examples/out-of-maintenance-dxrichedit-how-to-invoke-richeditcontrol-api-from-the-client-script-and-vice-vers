6dcf419ded4c25c1df755ca50145e4d0bf597b58
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/RichEditHTMLBridgeSL/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Browser;
+using System.Windows.Controls;
+
+namespace RichEditHTMLBridgeSL {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+
+            HtmlPage.RegisterScriptableObject("skPage", this);
+        }
+
+        [ScriptableMember()]
+        public void RichEditAppendText(string text) {
+            richEditControl1.Document.AppendText(text);
+        }
+
+        [ScriptableMember()]
+        public void RichEditCreate() {
+            richEditControl1.CreateNewDocument();
+        }
+
+        private void btnShowText_Click(object sender, System.Windows.RoutedEventArgs e) {
+            HtmlPage.Window.Invoke("showText", richEditControl1.Text);
+        }
+    }
+}
---
+++ VB/RichEditHTMLBridgeSL/MainPage.xaml.vb
@@ -0,0 +1,28 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows.Browser
+Imports System.Windows.Controls
+
+Namespace RichEditHTMLBridgeSL
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+
+			HtmlPage.RegisterScriptableObject("skPage", Me)
+		End Sub
+
+		<ScriptableMember()> _
+		Public Sub RichEditAppendText(ByVal text As String)
+			richEditControl1.Document.AppendText(text)
+		End Sub
+
+		<ScriptableMember()> _
+		Public Sub RichEditCreate()
+			richEditControl1.CreateNewDocument()
+		End Sub
+
+		Private Sub btnShowText_Click(ByVal sender As Object, ByVal e As System.Windows.RoutedEventArgs)
+			HtmlPage.Window.Invoke("showText", richEditControl1.Text)
+		End Sub
+	End Class
+End Namespace
